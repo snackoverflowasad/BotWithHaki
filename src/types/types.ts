2f07c9914eac910582ca43838d45a8a61caaa6a2
Asad-bot07 <asadhussain2408@gmail.com>
2026-02-19 05:20:31 +0530

    fix: updated types not used

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,8 @@
 export interface protocolType {
-    name: string;
-    allowGroupReplies: boolean;
-    allowBadWords: boolean;
-    description: string;
+  name: string;
+  allowGroupReplies: boolean;
+  allowBadWords: boolean;
+  description: string;
 }
 
 type ContactGroup = {
@@ -13,3 +13,23 @@ export type Contacts = {
   importants: ContactGroup;
   friends: ContactGroup;
 };
+
+// export type eventType = {
+//   summary: string;
+//   description?: string;
+//   start: {
+//     dateTime: string;
+//     timeZone: string;
+//   };
+//   end: {
+//     dateTime: string;
+//     timeZone: string;
+//   };
+//   reminders: {
+//     useDefault: boolean;
+//     overrides: {
+//       method: "popup" | "email";
+//       minutes: number;
+//     }[];
+//   };
+// };
