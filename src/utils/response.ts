febea09e1081a3288cfc4713ea18d2463b392538
Asad-bot07 <asadhussain2408@gmail.com>
2026-02-19 05:18:23 +0530

    feat: added response utils

---
+++ src/utils/response.ts
@@ -0,0 +1,25 @@
+import { google, calendar_v3 } from "googleapis";
+import { getAuth } from "../utils/googleAuth.js";
+
+type EventType = calendar_v3.Schema$Event;
+
+export const createEvent = async (
+  event: EventType
+): Promise<calendar_v3.Schema$Event> => {
+  const auth = await getAuth();
+
+  const cal = google.calendar({
+    version: "v3",
+    auth: auth as any,
+  });
+
+  const result = await cal.events.insert({
+    calendarId: "primary",
+    requestBody: event,
+    conferenceDataVersion: event.conferenceData ? 1 : 0,
+    sendNotifications: true,
+    sendUpdates: "all",
+  });
+
+  return result.data;
+};
